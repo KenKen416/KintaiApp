9b965133ca6e98305a13809911245422ea4b3e7b
KenKen <kenkenstudy416@gmail.com>
2025-09-24 13:59:57 +0900

    AttendanceCorrectioモデル作成

--- src/app/Models/AttendanceCorrection.php
+++ src/app/Models/AttendanceCorrection.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class AttendanceCorrection extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'attendance_id',
+        'requested_clock_in',
+        'requested_clock_out',
+        'requested_breaks',
+        'requested_note',
+        'status',
+    ];
+    protected $casts = [
+        'requested_clock_in' => 'datetime',
+        'requested_clock_out' => 'datetime',
+        'requested_breaks' => 'array',
+    ];
+    public function attendance()
+    {
+        return $this->belongsTo(Attendance::class);
+    }
 }
